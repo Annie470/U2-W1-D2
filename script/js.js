41c7e943e022aa6588e99aafa2dae284b0e9895e
Anna De Lise <anna_delise@libero.it>
2025-06-24 15:37:08 +0200

    add last exercise

--- script/js.js
+++ script/js.js
@@ -8,3 +8,13 @@ btnToTop.addEventListener("click", () => {
     top: 0,
   });
 });
+
+const btn1 = document.createElement("button");
+body.appendChild(btn1);
+btn1.innerHTML = `<i id="plus" class="far fa-plus-square"></i>`;
+btn1.classList.add("btn-plus");
+
+const btn2 = document.createElement("button");
+body.appendChild(btn2);
+btn2.innerHTML = `<i id="calendar" class="far fa-calendar-alt"></i>`;
+btn2.classList.add("btn-calendar");
